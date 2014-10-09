8a8d257f1f2f5d90bc7d2cc8bf6969a0037065a2
dclark87 <danieljclark87@gmail.com>
2014-10-09 16:22:41 -0400

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# setup.py
+#
+# Author: Daniel Clark, 2014
+
+'''
+This is the setup.py installation script to install the pytools package
+to the local disk.
+'''
+
+# Import packages
+from distutils.core import setup
+
+# Use disutils' setup function to install the package
+setup(name='py-tools',
+      version='0.0.1',
+      description='Various Python utilities',
+      author='Daniel Clark',
+      author_email='danieljclark87@gmail.com',
+      url='https://github.com/dclark87/py-tools',
+      packages=['pytools'])
