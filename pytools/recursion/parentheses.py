693cbd3e24955afb1b6e826a25a80f71d9e3618a
dclark87 <danieljclark87@gmail.com>
2016-10-06 12:08:27 -0400

    Added function to return all balanced pairs of parentheses

--- pytools/recursion/parentheses.py
+++ pytools/recursion/parentheses.py
@@ -36,3 +36,36 @@ def parenth_pairs(pairs):
     '''
 
     return list(_pairs(pairs, pairs))
+
+
+def _balanced(left, right, s, m):
+    '''
+    Recursive function to append to a list the balanced parentheses set
+    computed by keeping track of an open and closed count
+
+    :param left:
+    :param right:
+    :param s:
+    :return:
+    '''
+
+    if left == 0 and right == 0:
+        m.append(s)
+    if left > 0:
+        _balanced(left-1, right+1, s + '(', m)
+    if right > 0:
+        _balanced(left, right-1, s + ')', m)
+
+
+def return_balanced(num):
+    '''
+    Function to return a list of balanced parentheses given number of
+    pairs
+
+    :param num:
+    :return:
+    '''
+
+    out = []
+    _balanced(num, 0, '', out)
+    return out
